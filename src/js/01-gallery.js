05b19184ba08572a7dc06165ce2baf38d8ed69fe
IlyaGubinn <ilya.gubin29@gmail.com>
2023-01-26 17:24:20 +0300

    решение 1 задания

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
+// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+
+const cardPicctures = creetePicturesCard(galleryItems);
+
+
+
+function creetePicturesCard(item) {
+    return item.map(({ preview, original, description }) => {
+         return `<div class="gallery__item"><a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description}/>
+</a></div>`
+    }).join("");
+}
+
+galleryRef.innerHTML = cardPicctures
+new SimpleLightbox('.gallery a', { captions: true, captionsData: 'alt', captionDelay:250,});
+
+
+

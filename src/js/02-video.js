dba63e5c5f64fa2e63ecbdf22d312d4665aca29a
IlyaGubinn <ilya.gubin29@gmail.com>
2023-01-26 17:24:25 +0300

    решение 2 задания

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from "@vimeo/player"
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('#vimeo-player');
+    const player = new Vimeo(iframe);
+
+    
+const PLAYER_CURRENT_TIME = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(PLAYER_CURRENT_TIME, 1000));
+
+if (localStorage['videoplayer-current-time']) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+  
